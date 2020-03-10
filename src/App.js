ff3f9df8bad1cabc09d23e51ed73bfca5b491719
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-03-10 14:57:31 +0530

    Check the gitignore file working or not

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
         </p>
         <a
           className="App-link"
