afe72ac16d2d22a58f6719137a78ed85cb000e54
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-03-30 18:28:50 +0530

    Add the Responsive Meta Tag to index.html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 
 class App extends Component{
   render() {
     return(
         <div>
-
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
         </div>
     )
   }
