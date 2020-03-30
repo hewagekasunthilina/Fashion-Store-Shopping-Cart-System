02a58550e9d989f91c94f89cb87a3959219cacd7
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-03-30 18:14:51 +0530

    Install Material UI Dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <Button variant="secondary">Secondary</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
 
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return(
+        <div>
+
+        </div>
+    )
+  }
+
 }
 
 export default App;
