d81bb78620f65b6aac0ece902df20d9ecb9329cf
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-04-09 22:47:15 +0530

    Update the NavBar

--- src/Components/Common/NavBar/NavBar.jsx
+++ src/Components/Common/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const Navbar = (props) => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">Look @ Me</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
