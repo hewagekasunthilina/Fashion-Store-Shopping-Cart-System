94142d9d99ca01159557673d1a87e412afe0bbe2
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-04-11 22:43:31 +0530

    Add a new for to add the categories

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, {Component} from "react";
 
-import Template from "../Template/Template";
+class Admin extends Component{
+    constructor(props) {
+        super(props);
+        this.onChangeCategoryName = this.onChangeCategoryName.bind(this);
+        this.onChangeCategoryNumber = this.onChangeCategoryNumber.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
-const StorePanel = (props) => {
-    return (
-        <Template>
-            <h1>Admin Panel</h1>
-        </Template>
-    )
-};
+        this.state = {
+            category_name: '',
+            category_number: ''
+        }
+    }
 
-export default StorePanel;
+    onChangeCategoryName(e){
+        this.setState({
+            category_name: e.target.value
+        });
+    }
+
+    onChangeCategoryNumber(e){
+        this.setState({
+            category_number: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        console.log(`The values are ${this.state.category_name}, ${this.state.category_number}`)
+        this.setState({
+            category_name: '',
+            category_number: ''
+        })
+    }
+    render() {
+        return(
+            <div style={{marginTop: '10', marginLeft:'10%'}}>
+                <h3>Add New Category</h3>
+                <form  style={{width:'50%', marginLeft:'10%'}}>
+                    <div className="form-group">
+                        <label>New Category Name:  </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>New Category Number: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add Category" className="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+export default Admin;
