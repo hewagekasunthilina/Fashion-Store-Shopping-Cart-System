5cabed53b6f5dac001a0a77398f4fb23b4a3f705
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-04-14 23:21:44 +0530

    Datebase connection fixed

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 4000;
+const PORT = 3900;
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.listen(PORT, function(){
-    console.log('Server is running on Port:',PORT);
-});
+mongoose
+    .connect(
+        "mongodb+srv://FashionCluster:fashionstore500@fashioncluster-a2dyx.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        app.listen(3900);
+    })
+    .catch(error => {
+        console.log(error);
+    });
