68fe726ffc7e1bb017623492fbd7a90d8b9b38b7
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-04-25 20:29:18 +0530

    Fix the Mongo DB atlas issue successfully

--- src/Components/Admin/AddCategory.jsx
+++ src/Components/Admin/AddCategory.jsx
@@ -1,19 +1,22 @@
 import React, {Component} from "react";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
 
 class AddCategory extends Component{
+
 
     constructor(props) {
         super(props);
         this.state = {
+            id: '',
             category_name: '',
             category_number: ''
         };
 
         this.handleCategoryNameChange = this.handleCategoryNameChange.bind(this);
         this.handleCategoryNumberChange = this.handleCategoryNumberChange.bind(this);
+        this.handleCategoryIdChange = this.handleCategoryIdChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -34,42 +37,55 @@ class AddCategory extends Component{
             () => {
             });
     }
+    handleCategoryIdChange(e) {
 
-    handleSubmit(e) {
+        this.setState({
+                id: e.target.value
+            },
+            () => {
+            });
+    }
+
+
+    handleSubmit = async (e) => {
 
         e.preventDefault();
-
         const categoryObject = {
+            id: this.state.id,
             category_name: this.state.category_name,
             category_number: this.state.category_number
         };
 
+        const response = await fetch("http://localhost:4000/api/category", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(categoryObject)
+        });
+
+        const responseData = await response.json();
+        console.log(responseData);
+
+
+
+
         console.log(categoryObject);
 
-      axios.post(`${BASE_URL}/Backend/category`, categoryObject)
-          .then((res) => {
-              console.log(res.data);
-
-          if(res.data == true) {
-              alert('Category Added');
-          } else {
-              alert('Error');
-          }
-    }).catch((error)=> {
-        console.log(error)
-    });
-
-      this.setState({
-          category_name: '',
-          category_number: ''
-      })
-    }
+    };
     render() {
         return(
 
             <div style={{ marginLeft:'10%', marginTop:'8rem'}}>
                 <h3 style={{marginLeft: '24rem'}}>Add New Category</h3>
-                <form onSubmit={this.handleSubmit} action="/Backend/category" method="POST"  style={{width:'50%', marginLeft:'16rem'}}>
+                <form onSubmit={this.handleSubmit} action="/Backend/Routes/Fashion.routes.js" method="POST"  style={{width:'50%', marginLeft:'16rem'}}>
+                    <div className="form-group">
+                        <label>New Category Name:  </label>
+                        <input type="text"
+                               className="form-control"
+                               id="id"
+                               onChange={this.handleCategoryIdChange}/>
+                    </div>
                     <div className="form-group">
                         <label>New Category Name:  </label>
                         <input type="text"
