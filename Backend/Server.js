b483266e689db116336ac525718ea58b6556eb26
Pramoda Savindri <48210542+savi96@users.noreply.github.com>
2020-05-12 23:00:04 +0530

    half complete registrtion

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
+const passport = require("passport");
+const users = require("./Routes/Users.routs")
 
 mongoose
     .connect(
@@ -18,10 +20,14 @@ mongoose
         console.log(error);
     });
 
+
+
+app.use(passport.initialize());
+require("./Controller/passport")(passport);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use("/api", require('./Routes/Fashion.routes'));
 
-app.use("/api", require('./Routes/user.routes'));
-
+app.use("/api/users", users);
--- src/Routes.js
+++ src/Routes.js
@@ -11,17 +11,18 @@ import AddCategory from "./Components/Admin/AddCategory";
 import AddStoreManager from "./Components/Admin/AddStoreManager";
 import Payment from "./Components/User/Payment";
 
-import LoginButton from "./Components/Login/LoginButton";
+import Login from "./Components/UserRegistration/Login";
+import Register from "./Components/UserRegistration/Register";
 import UserProfile from "./Components/Profile/UserProfile";
 import CategoryList from "./Components/Admin/CategoryList";
+import UserDashbord from "./Components/Profile/UserDashbord";
 
 class Routes extends Component{
     render() {
         return (
             <Fragment>
-                {/************************** Admin Part ***************************/}
+                {/************************** Admin Part **************************/}
                 <Route path={"/"} exact component={Home}/>
-                <Route path={"/LoginButton"} exact component={LoginButton}/>
                 <Route path={"/Admin"} exact component={Admin}/>
                 <Route path={"/StorePanel"} exact component={StorePanel}/>
                 <Route path={"/AddToCart"} exact component={AddToCart}/>
@@ -31,6 +32,14 @@ class Routes extends Component{
                 <Route path={"/CategoryList"} exact component={CategoryList}/>
                 <Route path={"/Payment"} exact component={Payment}/>
                 <Route path={"/UserProfile"} exact component={UserProfile}/>
+
+                {/**********************************************************************/}
+
+                {/****************************User Registration************************************/}
+
+                <Route path={"/Login"} exact component={Login}/>
+                <Route path={"/Register"} exact component={Register}/>
+               <Route path={"/UserDashbord"} exact component={UserDashbord}/>
 
                 {/**********************************************************************/}
 
